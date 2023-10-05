feb6a240cab0683d2208e66d13678dbf8709507e
Riazantsev <riazantsevmark327@gmail.com>
2023-10-05 07:19:22 +0300

    testng added tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,34 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void couldRemainIfAmountZero() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int result = cashbackHackService.remain(0);
+
+        Assert.assertEquals(result, 1000);
+
+    }
+    @Test
+    public void couldRemainIfAmountPositive() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int result = cashbackHackService.remain(500);
+
+        Assert.assertEquals(result, 500);
+
+    }
+    @Test
+    public void couldRemainIfExistsBoundary() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int result = cashbackHackService.remain(2000);
+
+        Assert.assertEquals(result, 1000);
+
+    }
+}
